2aedca2f1f66542a182bde2e1b77970f0b1e7399
comfysage <67917529+comfysage@users.noreply.github.com>
2024-12-06 20:27:26 +0000

    fix(config/ivy)!: load plugins before rest of config

--- config/lua/ivy/init.lua
+++ config/lua/ivy/init.lua
@@ -1,11 +1,11 @@
 require("ivy.config.disable")
 
+require("lz.n").load("ivy.plugins")
+
 require("ivy.config.autocmds")
 require("ivy.config.ft")
 require("ivy.config.keybinds")
 require("ivy.config.neovide")
 require("ivy.config.options")
-
-require("lz.n").load("ivy.plugins")
 
 require("ivy.health").loaded = true
