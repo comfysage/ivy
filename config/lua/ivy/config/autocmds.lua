d3acc93755a51ab9880ac77c96eb1e4788c8a349
comfysage <67917529+comfysage@users.noreply.github.com>
2024-12-08 15:13:43 +0000

    feat(config/ivy/autocmds): add `TermOpen` autocmds

--- config/lua/ivy/config/autocmds.lua
+++ config/lua/ivy/config/autocmds.lua
@@ -26,3 +26,29 @@ vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "BufWinEnter", "WinEnte
     vim.api.nvim_command("startinsert")
   end,
 })
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "term://*",
+  callback = function()
+    vim.cmd("startinsert")
+  end,
+  desc = "start insert mode on TermOpen",
+})
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "term://*",
+  callback = function()
+    vim.opt_local.number = false
+  end,
+  desc = "remove line numbers",
+})
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "term://*",
+  desc = "create quit keymap",
+  callback = function(ev)
+    vim.keymap.set({ "t" }, "<c-x>q", function()
+      return [[<C-\><C-n>]]
+    end, { buffer = ev.buf, expr = true })
+  end,
+})
