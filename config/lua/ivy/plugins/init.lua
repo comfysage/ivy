ab5ec983d0766d338ecbbbc79f72cf76543bafe9
comfysage <67917529+comfysage@users.noreply.github.com>
2024-12-08 10:44:01 +0000

    feat(config/ivy/plugins): setup `mini.files`

--- config/lua/ivy/plugins/init.lua
+++ config/lua/ivy/plugins/init.lua
@@ -117,7 +117,25 @@ return {
     "mini.files",
     event = "UIEnter",
     after = function()
-      require("mini.files").setup()
+      require("mini.files").setup({
+        options = {
+          use_as_default_explorer = false,
+        },
+        windows = {
+          -- Maximum number of windows to show side by side
+          max_number = 3,
+          -- Whether to show preview of file/directory under cursor
+          preview = false,
+          -- Width of focused window
+          width_focus = 50,
+          -- Width of non-focused window
+          width_nofocus = 15,
+          -- Width of preview window
+          width_preview = 25,
+        },
+      })
+      local keymaps = require("keymaps").setup()
+      keymaps.normal["<space>sp"] = { require("mini.files").open, "Open Files", group = "UI" }
     end,
   },
   {
