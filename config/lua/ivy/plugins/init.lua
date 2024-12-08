861e9c7634ad3e5fe0dd29565cd2221ac27c4e0a
comfysage <67917529+comfysage@users.noreply.github.com>
2024-12-08 10:43:19 +0000

    feat(config/ivy/plugins): setup `mini.diff`

--- config/lua/ivy/plugins/init.lua
+++ config/lua/ivy/plugins/init.lua
@@ -80,7 +80,37 @@ return {
     "mini.diff",
     event = "BufAdd",
     after = function()
-      require("mini.diff").setup()
+      require("mini.diff").setup({
+        view = {
+          style = "sign",
+          signs = {
+            add = "│",
+            change = "│",
+            delete = "│",
+            topdelete = "‾",
+            changedelete = "~",
+            untracked = "┆",
+          },
+        },
+        delay = { text_change = 1000 },
+        mappings = {
+          -- Apply hunks inside a visual/operator region
+          apply = "gh",
+
+          -- Reset hunks inside a visual/operator region
+          reset = "gH",
+
+          -- Hunk range textobject to be used inside operator
+          -- Works also in Visual mode if mapping differs from apply and reset
+          textobject = "gh",
+
+          -- Go to hunk range in corresponding direction
+          goto_first = "[H",
+          goto_prev = "[h",
+          goto_next = "]h",
+          goto_last = "]H",
+        },
+      })
     end,
   },
   {
