ec1a69ab7ca06562b2eedb6838cc87423527029e
comfysage <67917529+comfysage@users.noreply.github.com>
2024-12-08 10:43:08 +0000

    feat(config/ivy/plugins): setup `mini.ai`

--- config/lua/ivy/plugins/init.lua
+++ config/lua/ivy/plugins/init.lua
@@ -36,7 +36,23 @@ return {
     "mini.ai",
     event = "BufAdd",
     after = function()
-      require("mini.ai").setup()
+      require("mini.ai").setup({
+        mappings = {
+          -- Main textobject prefixes
+          around = "a",
+          inside = "i",
+
+          -- Next/last variants
+          around_next = "an",
+          inside_next = "in",
+          around_last = "al",
+          inside_last = "il",
+
+          -- Move cursor to corresponding edge of `a` textobject
+          goto_left = "g[",
+          goto_right = "g]",
+        },
+      })
     end,
   },
   {
