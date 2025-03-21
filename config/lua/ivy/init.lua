e445dedf553d28f25ff918ce44f7d7dedc02d3d4
robin <comfysage@isabelroses.com>
2025-03-21 23:19:58 +0000

    fix(config/ivy): remove custom lsp keymaps

--- config/lua/ivy/init.lua
+++ config/lua/ivy/init.lua
@@ -166,17 +166,7 @@ require("lz.n").load({
               end
 
               local opts = { buffer = ev.buf }
-              local function use_border(cb)
-                return function()
-                  cb({ border = vim.g.bc.style })
-                end
-              end
               vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-              vim.keymap.set("n", "K", use_border(vim.lsp.buf.hover), opts)
-              vim.keymap.set("i", "<C-k>", use_border(vim.lsp.buf.signature_help), opts)
-              vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-              vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-              vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
               vim.keymap.set("n", "<localleader>f", function()
                 vim.lsp.buf.format({ async = true })
               end, opts)
