7070c2f0a08b07f8d2e5a4b9ace89cf61918b0ec
robin <comfysage@isabelroses.com>
2025-03-22 22:35:34 +0000

    fix(config/lsp/fennel_ls): use settings - modify fennel path - add globals

--- config/lsp/fennel_ls.lua
+++ config/lsp/fennel_ls.lua
@@ -2,7 +2,10 @@ return {
   cmd = { 'fennel-ls' },
   filetypes = { 'fennel' },
   root_markers = { 'flsproject.fnl', '.git' },
-  settings = {},
+  settings = {
+    ['fennel-path'] = "./?.fnl;./?/init.fnl;src/?.fnl;src/?/init.fnl;fnl/?.fnl;fnl/?/init.fnl",
+    ['extra-globals'] = "vim package table",
+  },
   single_file_support = true,
   capabilities = {
     offsetEncoding = { 'utf-8', 'utf-16' },
