ed322e494f1f49adfb2171ef2e929a70140c729d
isabel <isabel@isabelroses.com>
2025-07-22 12:34:25 +0100

    feat: use gift-wrap

--- default.nix
+++ default.nix
@@ -8,14 +8,15 @@
   system ? builtins.currentSystem,
 
   # wow this is hacky
-  self ? { },
+  inputs,
+  self ? inputs.self,
   ivyVersion ? self.shortRev or self.dirtyRev or "unknown",
 }:
 let
   packages = lib.makeScope pkgs.newScope (self: {
     ivy = self.callPackage ./pkgs/ivy/package.nix { inherit ivyVersion; };
     ivyPlugins = self.callPackage ./pkgs/ivy-plugins/package.nix { };
-    wrapNeovim = self.callPackage ./pkgs/wrap-neovim/package.nix { };
+    inherit (inputs.gift-wrap.legacyPackages.${pkgs.stdenv.hostPlatform.system}) wrapNeovim;
   });
 in
 packages
