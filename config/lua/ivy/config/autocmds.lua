2382dc43e7af3cc408eb3b7e57583d120edf3095
robin <comfysage@isabelroses.com>
2025-07-25 23:15:53 +0200

    fix(autocmds): yank hl group

--- config/lua/ivy/config/autocmds.lua
+++ config/lua/ivy/config/autocmds.lua
@@ -9,7 +9,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("editor:yank:highlight", { clear = true }),
   pattern = "*",
   callback = function()
-    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+    vim.highlight.on_yank({ higroup = "CurSearch", timeout = 200 })
   end,
   desc = "Highlight yanked text",
 })
