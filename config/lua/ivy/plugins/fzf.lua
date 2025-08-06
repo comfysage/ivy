1bd8a4694654c1ae5a9cfbd386a05e31e2d7fc50
robin <comfysage@isabelroses.com>
2025-08-06 13:22:46 +0200

    feat(fzf): reconfigure ivy style

--- config/lua/ivy/plugins/fzf.lua
+++ config/lua/ivy/plugins/fzf.lua
@@ -3,39 +3,74 @@ return {
     "fzf-lua",
     event = "DeferredUIEnter",
     after = function()
-      vim.api.nvim_set_hl(0, "FzfLuaBorder", { link = "FloatBorder" })
+      local theme = {
+        { "default-title" },
+        winopts = {
+          row = 1,
+          col = 0,
+          width = 1,
+          height = 0.4,
+          backdrop = 100,
+          title_pos = "left",
+          border = { "", "─", "", "", "", "", "", "" },
+          preview = {
+            layout = "horizontal",
+            title_pos = "right",
+            border = function(_, m)
+              if m.type == "fzf" then
+                return "single"
+              else
+                assert(m.type == "nvim" and m.name == "prev" and type(m.layout) == "string", "no border set")
+                local b = { "┌", "─", "┐", "│", "┘", "─", "└", "│" }
+                if m.layout == "down" then
+                  b[1] = "├" --top right
+                  b[3] = "┤" -- top left
+                elseif m.layout == "up" then
+                  b[7] = "├" -- bottom left
+                  b[6] = "" -- remove bottom
+                  b[5] = "┤" -- bottom right
+                elseif m.layout == "left" then
+                  b[3] = "┬" -- top right
+                  b[5] = "┴" -- bottom right
+                  b[6] = "" -- remove bottom
+                else -- right
+                  b[1] = "┬" -- top left
+                  b[7] = "┴" -- bottom left
+                  b[6] = "" -- remove bottom
+                end
+                return b
+              end
+            end,
+          },
+        },
+      }
 
-      require("fzf-lua").setup({
-        winopts = function(opts)
-          opts = opts or {}
-          local size = 30
-          local preview_size = size / 2
-          if opts.previewer == false then
-            size = size / 2
-          end
-          local bottom_border = { vim.g.bc.horiz, vim.g.bc.horiz, vim.g.bc.horiz, "", "", "", "", "" }
-          return {
-            height = size,
-            width = 1.0,
-            col = 0,
-            row = vim.o.lines - size - 1 - vim.o.cmdheight, -- offset to avoid covering statusline
-            preview = {
-              winopts = {
-                cursorline = false,
-              },
-              layout = "vertical",
-              vertical = "up:" .. preview_size,
-              title_pos = "left",
-            },
-            backdrop = 100,
-            border = bottom_border,
-          }
-        end,
+      local up = {
+        row = 1,
+        col = 0,
+        width = 1,
+        height = 1,
+        preview = {
+          layout = "vertical",
+          vertical = "up:50%",
+          border = "none",
+        },
+      }
+
+      theme.blines = { winopts = up, previewer = { toggle_behavior = "extend" } }
+      theme.lines = theme.blines
+      theme.grep = theme.blines
+      theme.grep_curbuf = theme.blines
+      theme.git = { blame = { winopts = up } }
+
+      local opts = {
         fzf_opts = {
           ["--layout"] = "reverse-list",
           ["--info"] = "inline-right",
           ["--no-separator"] = "",
+          ["--padding"] = "0",
         },
+        fzf_colors = true,
         file_icon_padding = " ",
         prompt = " ",
         previewer = "builtin",
@@ -49,56 +84,61 @@ return {
             treesitter = true,
           },
         },
+        keymaps = {
+          previewer = false,
+        },
         file_ignore_patterns = {
-          "%.npz",
-          "%.pyc",
-          "%.luac",
-          "%.ipynb",
-          "vendor/*",
-          "%.lock",
-          "__pycache__/*",
-          "%.sqlite3",
-          "%.ipynb",
-          "node_modules/*",
-          "%.min.js",
-          "%.jpg",
-          "%.jpeg",
-          "%.png",
           "%.age",
-          "%.svg",
-          "%.otf",
-          "%.ttf",
-          "%.git/",
-          "%.webp",
+          "%.cache",
+          "%.class",
           "%.dart_tool/",
+          "%.dll",
+          "%.docx",
+          "%.dylib",
+          "%.exe",
+          "%.git/",
           "%.gradle/",
+          "%.ico",
           "%.idea/",
+          "%.ipynb",
+          "%.jar",
+          "%.jpeg",
+          "%.jpg",
+          "%.lock",
+          "%.luac",
+          "%.met",
+          "%.min.js",
+          "%.npz",
+          "%.otf",
+          "%.pdb",
+          "%.pdf",
+          "%.png",
+          "%.pyc",
           "%.settings/",
+          "%.so",
+          "%.sqlite3",
+          "%.ttf",
+          "%.vale/",
           "%.vscode/",
+          "%.webp",
+          ".direnv/",
+          ".direnv/*",
           "__pycache__/",
+          "__pycache__/*",
+          "_sources/",
           "build/",
           "env/",
           "gradle/",
           "node_modules/",
-          "target/",
-          "%.pdb",
-          "%.dll",
-          "%.class",
-          "%.exe",
-          "%.so",
-          "%.cache",
-          "%.ico",
-          "%.pdf",
-          "%.dylib",
-          "%.jar",
-          "%.docx",
-          "%.met",
+          "node_modules/*",
           "smalljre_*/*",
-          "%.vale/",
-          "_sources/",
+          "target/",
           "tmp/",
+          "vendor/*",
         },
-      })
+      }
+
+      require("fzf-lua").setup(vim.tbl_deep_extend("force", theme, opts))
 
       require("fzf-lua").register_ui_select()
 
