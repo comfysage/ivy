2fca218493d2990b929a0fd3b2d9b00fdfb60de8
robin <comfysagedev@gmail.com>
2025-11-20 20:00:45 +0100

    artio: add opts

--- config/config/artio.lua
+++ config/config/artio.lua
@@ -3,6 +3,16 @@ vim.ui.select = function(...)
   return require("artio").select(...)
 end
 
+require("artio").setup({
+  opts = {
+    shrink = false,
+    prompt_title = false,
+  },
+  win = {
+    height = 0.3,
+  },
+})
+
 vim.keymap.set("n", "<leader><leader>", "<Plug>(artio-files)")
 vim.keymap.set("n", "<leader>fh", "<Plug>(artio-helptags)")
 vim.keymap.set("n", "<leader>fb", "<Plug>(artio-buffers)")
