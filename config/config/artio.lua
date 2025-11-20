e142acf7c90fbe8df471801f6d566f16bd062f68
robin <comfysagedev@gmail.com>
2025-11-20 13:46:57 +0100

    artio: add keymaps for builtins

--- config/config/artio.lua
+++ config/config/artio.lua
@@ -1,5 +1,8 @@
+---@diagnostic disable-next-line: duplicate-set-field
 vim.ui.select = function(...)
   return require("artio").select(...)
 end
 
 vim.keymap.set("n", "<leader><leader>", "<Plug>(artio-files)")
+vim.keymap.set("n", "<leader>fh", "<Plug>(artio-helptags)")
+vim.keymap.set("n", "<leader>fb", "<Plug>(artio-buffers)")
