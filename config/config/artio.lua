0b9b21f21da97a01f50eba0971b38ade68430dd4
robin <comfysagedev@gmail.com>
2025-11-28 00:43:38 +0100

    artio: add zoxide keymap

--- config/config/artio.lua
+++ config/config/artio.lua
@@ -18,3 +18,23 @@ vim.keymap.set("n", "<leader>fg", "<Plug>(artio-grep)")
 vim.keymap.set("n", "<leader>fh", "<Plug>(artio-helptags)")
 vim.keymap.set("n", "<leader>fb", "<Plug>(artio-buffers)")
 vim.keymap.set("n", "<leader>f/", "<Plug>(artio-buffergrep)")
+
+vim.keymap.set("n", "<leader>fz", function()
+  local lst = require("artio.utils").make_cmd("zoxide query -l")()
+
+  require("artio").generic(lst, {
+    on_close = function(item, _)
+      vim.schedule(function()
+        vim.cmd.cd(item)
+      end)
+    end,
+    get_icon = function(item)
+      return require("mini.icons").get("directory", item.v)
+    end,
+    hl_item = function(item)
+      return {
+        { { 0, #item.v }, "Directory" },
+      }
+    end,
+  })
+end)
