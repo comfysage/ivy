d0dbae0edd609057603750b3fdc5e4d330429457
robin <comfysagedev@gmail.com>
2025-11-27 15:45:34 +0100

    options: add `--` to end of `grepformat`

--- config/lua/ivy/config/options.lua
+++ config/lua/ivy/config/options.lua
@@ -134,9 +134,9 @@ vim.o.cmdheight = 0
 vim.o.laststatus = 3
 
 -- use rg for grepping
-vim.o.grepprg = vim.fn.executable("rg") == 1 and "rg --vimgrep" or "grep -n $* /dev/null"
+vim.o.grepprg = vim.fn.executable("rg") == 1 and "rg --vimgrep --" or "grep -n $* /dev/null"
 vim.o.grepformat = "%f:%l:%c:%m"
-vim.g.findprg = vim.fn.executable("fd") == 1 and "fd -p -t f --color=never" or "find . -type f"
+vim.g.findprg = vim.fn.executable("fd") == 1 and "fd -p -t f --color=never --" or "find . -type f"
 
 -- let me have spelling checking for english
 vim.opt.spelllang = { "en" }
